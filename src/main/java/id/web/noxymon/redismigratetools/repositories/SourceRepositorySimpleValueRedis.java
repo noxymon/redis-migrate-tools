0b4b55ef487ee0be0377b6c44df33203d96ed888
noxymon <avicienna.ulhaq@gmail.com>
2020-06-11 00:22:52 +0700

    Change to reactive

--- src/main/java/id/web/noxymon/redismigratetools/repositories/SourceRepositorySimpleValueRedis.java
+++ src/main/java/id/web/noxymon/redismigratetools/repositories/SourceRepositorySimpleValueRedis.java
@@ -2,29 +2,41 @@ package id.web.noxymon.redismigratetools.repositories;
 
 import id.web.noxymon.redismigratetools.service.RedisMigrationSaver;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.redis.core.ReactiveRedisTemplate;
 import org.springframework.data.redis.core.ScanOptions;
-import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.stereotype.Repository;
 
+import java.io.IOException;
+
+@Slf4j
 @Repository
 @RequiredArgsConstructor
 public class SourceRepositorySimpleValueRedis
 {
     private final RedisMigrationSaver redisMigrationSaver;
-    private final StringRedisTemplate sourceRedisTemplate;
+    private final ReactiveRedisTemplate reactiveRedisTemplate;
 
     public void migrate(String pattern)
     {
-        //final Long currentDbSize = sourceRedisTemplate.getConnectionFactory().getConnection().dbSize();
-        ScanOptions options = ScanOptions.scanOptions()
-                                         .match(pattern)
-                                         .build();
-        sourceRedisTemplate
-                .getConnectionFactory()
-                .getConnection()
-                .scan(options)
-                .forEachRemaining(content -> {
-                    redisMigrationSaver.migrateKey(content);
-                });
+        ScanOptions options = buildScanOptions(pattern);
+        reactiveRedisTemplate.getConnectionFactory()
+                             .getReactiveConnection()
+                             .keyCommands()
+                             .scan(options)
+                             .subscribe(byteBuffer -> {
+                                 try {
+                                     redisMigrationSaver.migrateKey(byteBuffer.array());
+                                 } catch (IOException e) {
+                                     log.error("Error : " + e.getMessage());
+                                 }
+                             });
+    }
+
+    private ScanOptions buildScanOptions(String pattern)
+    {
+        return ScanOptions.scanOptions()
+                          .match(pattern)
+                          .build();
     }
 }
--- src/main/java/id/web/noxymon/redismigratetools/service/RedisMigrationSaver.java
+++ src/main/java/id/web/noxymon/redismigratetools/service/RedisMigrationSaver.java
@@ -2,13 +2,15 @@ package id.web.noxymon.redismigratetools.service;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.io.FileUtils;
 import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.scheduling.annotation.Async;
 import org.springframework.scheduling.annotation.AsyncResult;
 import org.springframework.stereotype.Service;
 
+import java.io.File;
+import java.io.IOException;
 import java.util.concurrent.Future;
-import java.util.concurrent.TimeUnit;
 
 @Slf4j
 @Service
@@ -16,22 +18,12 @@ import java.util.concurrent.TimeUnit;
 public class RedisMigrationSaver
 {
     private final StringRedisTemplate sourceRedisTemplate;
-    private final StringRedisTemplate targetRedisTemplate;
 
     @Async
-    public Future<Void> migrateKey(byte[] content)
+    public Future<Void> migrateKey(byte[] content) throws IOException
     {
-        final String redisKey = new String(content);
-        final byte[] dump = sourceRedisTemplate.dump(redisKey);
-        if(dump != null || dump.length > 0){
-            targetRedisTemplate
-                    .restore(
-                            redisKey, dump,
-                            0, TimeUnit.DAYS,
-                            true
-                    );
-            log.info("Hasilnya : " + redisKey);
-        }
+        final byte[] dump = sourceRedisTemplate.dump(new String(content));
+        FileUtils.writeByteArrayToFile(new File("generated/gen.dump"), dump);
         return new AsyncResult<Void>(null);
     }
 }
